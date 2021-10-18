d4a4609dc47a8673cf2761cf3feda37c43ac4a76
Ling Yi-Shan <tompoo.2015@gmail.com>
2021-10-18 14:12:50 +0800

    Initial Commit

---
+++ shapecapoo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shapecapoo
+//
+//  Created by 凌翊瀗 on 2021/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shapecapoo/shapecapooApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapecapooApp.swift
+//  shapecapoo
+//
+//  Created by 凌翊瀗 on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct shapecapooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
